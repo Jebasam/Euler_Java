507bd49ed596c68f83e0730c4ab50ee3fbb33e84
Jebasam <jebasam.official@gmail.com>
2021-09-13 23:07:52 +0530

    Push
    Initial Push

---
+++ Euler_java/src/multiples_of_3or5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class multiples_of_3or5 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n=scanner.nextInt();
+		int sum=0;
+		for(int i=1;i<n;i++) {
+			if((i%3==0) || (i%5==0)) {
+				sum = sum+i;
+			}
+		}
+		System.out.println(sum);
+	}
+
+}
